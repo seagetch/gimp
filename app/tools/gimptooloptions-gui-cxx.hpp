941400e17f3746cf12c314a977b17d773f658c59
sigetch <sigetch@gmail.com>
2017-03-06 00:50:31 +0900

    - Updates some code for new delegator implementation.

--- app/tools/gimptooloptions-gui-cxx.hpp
+++ app/tools/gimptooloptions-gui-cxx.hpp
@@ -20,7 +20,7 @@
 
 #include "base/delegators.hpp"
 
-typedef Delegator::Delegator<void (*)(GObject*,GtkWidget**)> PopupCreateViewDelegator;
+typedef Delegator::Delegator<void, GObject*,GtkWidget**> PopupCreateViewDelegator;
 
 GtkWidget * gimp_tool_options_button_with_popup (GtkWidget                *label_widget,
                                                  PopupCreateViewDelegator* create_view);
